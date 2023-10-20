fdc23c88e1209043643aad81fe0923135450e9ca
smitpatelx <smitbpatel1999@gmail.com>
2023-10-20 01:10:23 -0500

    Routers added.

--- src/router/router.go
+++ src/router/router.go
@@ -2,14 +2,18 @@ package router
 
 import (
 	"github.com/gofiber/fiber/v2"
+	actor "github.com/smitpatelx/neurelo-go-htmx-example/src/services/actor"
 )
 
 func SetupRoutes(app *fiber.App) {
-	// --- API groups   ---
-	// api_1 := app.Group("/api/v1")
+	// --- Render pages ---
+	app.Get("/", actor.RenderIndexPage)
+	app.Get("/actors", actor.GetAllActors)
+	app.Get("/films", actor.GetAllFilms)
 
-	// Render homepage
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{})
-	})
+	// --- API groups   ---
+	api_1 := app.Group("/api/v1")
+
+	// --- Actors API ---
+	api_1.Get("/actors", actor.GetAllActorsAPI)
 }
