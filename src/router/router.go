738f4c12b95c913adfd521c9b5f483bac86d2d30
smitpatelx <smitbpatel1999@gmail.com>
2023-10-21 15:21:50 -0500

    Api route for film api added.

--- src/router/router.go
+++ src/router/router.go
@@ -3,17 +3,19 @@ package router
 import (
 	"github.com/gofiber/fiber/v2"
 	actor "github.com/smitpatelx/neurelo-go-htmx-example/src/services/actor"
+	film "github.com/smitpatelx/neurelo-go-htmx-example/src/services/film"
 )
 
 func SetupRoutes(app *fiber.App) {
 	// --- Render pages ---
 	app.Get("/", actor.RenderIndexPage)
 	app.Get("/actors", actor.GetAllActors)
-	app.Get("/films", actor.GetAllFilms)
+	app.Get("/films", film.GetAllFilms)
 
 	// --- API groups   ---
 	api_1 := app.Group("/api/v1")
 
-	// --- Actors API ---
+	// --- APIs ---
 	api_1.Get("/actors", actor.GetAllActorsAPI)
+	api_1.Get("/films", film.GetAllFilmsAPI)
 }
