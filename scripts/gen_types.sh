1a61f91c6f59d662bcdf616b1623483cb7953d0f
smitpatelx <smitbpatel1999@gmail.com>
2023-10-26 23:09:17 -0500

    Open api generator config changed. Errors reduced to 71 from 1000 plus. Still client wasnt usable.

--- scripts/gen_types.sh
+++ scripts/gen_types.sh
@@ -1,8 +1,9 @@
 # Generate types
 spec_file="./specs/booking-system-859e7e1cf8e5-rest.json"
-output_dir="./src/oapi.gen.go"
+output_dir="./openapi/"
 config_file="./openapi-config.json"
 
 rm -rf $output_dir
-# openapi-generator generate -i $spec_file -g go -o $output_dir -c $config_file
-oapi-codegen -package oapi $spec_file > $output_dir
+openapi-generator generate -i $spec_file -g go -o $output_dir -c $config_file
+# openapi-generator validate -i $spec_file
+# oapi-codegen -package oapi $spec_file > $output_dir
